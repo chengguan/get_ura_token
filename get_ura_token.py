dbbfb37c5235beaf804531009714332b84eff58a
Cheng Guan Teo <chengguan.teo@gmail.com>
2020-05-19 13:21:47 +0800

    Update get_ura_token.py

--- get_ura_token.py
+++ get_ura_token.py
@@ -1,10 +1,17 @@
+# get_ura_token.py
+# 19 May 2020
+#
+# Register for your access key here: https://www.ura.gov.sg/maps/api/reg.html
+# Questions or bugs, please write to chengguan.teo@gmail.com.
 import requests
 
+# Paste your access key from URA here. THe access key is valid for 1 year.
 accesskey = "<paste your access key here>"
 
 url = 'https://www.ura.gov.sg/uraDataService/insertNewToken.action'
 myobj = {'AccessKey': accesskey}
 
+# Place the access key in the request header and send.
 resp = requests.post(url, headers=myobj)
 
 if (resp.status_code==200):
@@ -12,6 +19,6 @@ if (resp.status_code==200):
         result = resp.json()['Result']
         print(f"Token: '{result}'.")
     except:
-        print(resp.text)
+        print(resp.text) # print error msg (should the resp data structure changed...)
 else:
-    print(resp.text)
+    print(resp.text) # print error msg
